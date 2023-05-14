a43402359abc5a84dfe3f65f4170216f2584a2a2
Uzo Nwokoro <uzo.tammy@gmail.com>
2023-05-14 18:13:57 -0300

    Feat: Real-time data update
    - real-time communication
    from server (json file) to javascript.
    - Using HttpResponse flexibility

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, GetTimeView, StreamView
+from . import views
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('stream/', StreamView.as_view(), name='stream'),
+    path('get_time/', GetTimeView.as_view(), name='get_time'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,49 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+# from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView, View
+import time, datetime
+import json
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'core/index.html'
+
+    
+
+class StreamView(TemplateView): 
+    template_name = 'core/event_stream.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        try:
+            with open('core/static/time.json') as rf:
+                content = json.load(rf)
+            context['dt'] = content['time']
+        except:
+            content = ''
+        
+        return context
+
+class GetTimeView(View):    
+    def get(self, request):
+        def timer():
+            with open('core/static/time.json', 'w') as wf:
+                """get the time"""
+                ticker = time.gmtime()
+                # Extract day, hour, minutes, and seconds
+                data = {
+                    "time": {"day": str(ticker.tm_mday).zfill(2),
+                             "hour": str(ticker.tm_hour).zfill(2),
+                             "minutes": str(ticker.tm_min).zfill(2),
+                             "seconds": str(ticker.tm_sec).zfill(2)
+                            },
+                        }
+                json.dump(data, wf, indent=2)
+                
+            time.sleep(1)
+        
+        response = HttpResponse(timer(), content_type='text/event-stream')
+        response['Cache-Control'] = 'no-cache'
+        return response
+    
+    
